17e0814913a7ccf92b961f7ce0e0920d0858b00e
MABadge <mabadgeley@gmail.com>
2019-09-25 21:55:18 -0400

    command to download all ~gitlab libR group repos

---
+++ bin/clone_all_lib_repos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+git clone git@git.nak.co:libR/MyUtils.git
+git clone git@git.nak.co:libR/vizR.git
+git clone git@git.nak.co:libR/MlAnalysis.git
+git clone git@git.nak.co:libR/R-DlAnalysis.git
+git clone git@git.nak.co:libR/AnalysisToolkit.git
+git clone git@git.nak.co:libR/thirdparty.git
+git clone git@git.nak.co:libR/testr.git
