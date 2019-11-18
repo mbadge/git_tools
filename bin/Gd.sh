19393f532193c7e3b3b56468ec8ac2e783799c2c
MABadge <mabadgeley@gmail.com>
2019-11-17 23:31:16 -0500

    add `Gd.sh` and `gd.sh` to show all unstaged changes. #3

--- bin/Gd.sh
+++ bin/Gd.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-set -Eeuxo pipefail
+set -Eeuo pipefail
 IFS=$'\n\t'
 
 #/ Usage: Gd.sh
-#/ Description: Show differences in working directory
+#/ Description: Show unstaged changes in in working directory
 #/ Options:
 #/   --help: Display this help message
 usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
 expr "$*" : ".*--help" > /dev/null && usage
 
-# TODO: figure out how to include untrackied but not ignored files also
+# include all unstaged files by indexing them as a new file
+fp_unstaged=$( git ls-files --others --exclude-standard )
+echo ${fp_unstaged[@]} | xargs git add -N
+
+# watch all unstaged changes
 watch --color -n1 --no-title \
     "git diff"
