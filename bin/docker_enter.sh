7c0b297f600be82bd46a539602587a77d8d102d7
MABadge <mabadgeley@gmail.com>
2019-11-27 21:55:42 -0500

    util to enter a ~docker container (bash)

---
+++ bin/docker_enter.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+#/ Usage: docker_enter CONTAINER
+#/ Description: wrapper for `docker exec -il CONTAINER bash`
+#/ Options:
+#/   --help: Display this help message
+usage() { grep '^#/' "$0" | cut -c4- ; echo -e "\n running containers:"; docker ps; exit 0 ; }
+expr "$*" : ".*--help" > /dev/null && usage
+
+# CLI
+if [ "$#" -lt 1 ] ; then
+    echo -e "ERROR: Please provide argument CONTAINER\n"
+    usage
+    echo -e "\nHere are your running containers:"
+fi
+
+# PARAMS
+CONTAINER=$1
+
+# MAIN
+docker exec -it ${CONTAINER} bash
